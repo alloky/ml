6f5e1a68647b37b012c4900cc8cc7e325fbf78ba
alloky <alloky2011@gmail.com>
2018-03-24 14:16:45 +0300

    evel

--- 2_lec/kaggle/eval.py
+++ 2_lec/kaggle/eval.py
@@ -37,7 +37,7 @@ def eval(options):
 
     # загружаем сеть
     cp_dic = torch.load(options.model)
-    print(cp_dic)
+    print([*cp_dic])
     net.load_state_dict(cp_dic['net'])
 
     transform_test = transforms.Compose([
