a018bd8f64aac3e754e025a143473e13f113eca3
alloky <alloky2011@gmail.com>
2018-03-24 23:44:15 +0300

    decay

--- 2_lec/kaggle/train.py
+++ 2_lec/kaggle/train.py
@@ -107,7 +107,7 @@ def train(options):
     criterion = nn.CrossEntropyLoss()
     # тут создаем оптимайзер, который нужен
     learning_rate = 1e-2
-    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-6) # 
+    optimizer = torch.optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9, weight_decay=1e-6, nesterov=True) # 
 
     start_from_epoch = 0
     # Если указан чекпойнт то загружаем сеть
