0b2723e41d2acfed71d88b14f436e35176a1a058
alloky <alloky2011@gmail.com>
2018-03-25 02:16:27 +0300

    .

--- 2_lec/kaggle/cifar_model.py
+++ 2_lec/kaggle/cifar_model.py
@@ -24,13 +24,13 @@ class Net(nn.Module):
         self.net.add_module('do_4', nn.Dropout(0.5))
         
         
-        self.fc1 = nn.Linear(32 * 4 * 4, 120)
+        self.fc1 = nn.Linear(32 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
     def forward(self, x):
         x = self.net(x)
-        x = x.view(-1, 16 * 5 * 5)
+        x = x.view(-1, 32 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
