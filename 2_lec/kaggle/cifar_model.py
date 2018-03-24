b95624cab3942094e42c5981ca8989282475c91e
alloky <alloky2011@gmail.com>
2018-03-24 21:43:46 +0300

    refactor

--- 2_lec/kaggle/cifar_model.py
+++ 2_lec/kaggle/cifar_model.py
@@ -9,9 +9,15 @@ class Net(nn.Module):
     # Слои, в которых нет параметров для обучения можно не создавать, а брать из переменной F
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.net = nn.Sequential(nn.BatchNorm2d(3))
+        self.net.add_module('conv_1', nn.Conv2d(3, 6, 5))
+        self.net.add_module('bn_1', nn.BatchNorm2d(6))
+        self.net.add_module('do_1', nn.Dropout(0.5))
+        self.net.add_module('relu_1', nn.ReLU())
+        self.net.add_module('pool_1', nn.MaxPool2d(2, 2))
+        self.net.add_module('conv_2', nn.Conv2d(6, 16, 5))
+        self.net.add_module('relu_2', nn.ReLU())
+        self.net.add_module('pool_2', nn.MaxPool2d(2, 2))
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
