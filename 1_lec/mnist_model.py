b4206ef54aa95974dfb504f705d4162514136a06
alloky <alloky2011@gmail.com>
2018-03-31 13:28:17 +0300

    .

--- 1_lec/mnist_model.py
+++ 1_lec/mnist_model.py
@@ -30,6 +30,6 @@ class Net(nn.Module):
         self.net.add_module('sf_5', nn.Softmax())
 
     def forward(self, x):
-        print(x.shape())
+        print(x.shape)
         x = self.net(x)
         return x
