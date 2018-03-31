98a0c91670be0e424cfb84f7e2c1f8b9af538426
alloky <alloky2011@gmail.com>
2018-03-31 22:35:38 +0300

    .

--- 1_lec/eval.py
+++ 1_lec/eval.py
@@ -49,7 +49,6 @@ def eval(options):
     net.load_state_dict(cp_dic)
 
     transform_test = transforms.Compose([
-        transforms.ToTensor(),
        transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                  std=[0.229, 0.224, 0.225]) #нормализация данных
             
